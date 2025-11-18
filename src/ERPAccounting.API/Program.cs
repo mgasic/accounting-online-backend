94beb5b7c699a0b3a6d53a344012526dd330b4b0
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-11-18 03:15:25 +0100

    Add in-memory EF provider registration

--- src/ERPAccounting.API/Program.cs
+++ src/ERPAccounting.API/Program.cs
@@ -4,11 +4,8 @@ using ERPAccounting.Application.DTOs.LineItems;
 using ERPAccounting.Application.Services;
 using ERPAccounting.Application.Services.Contracts;
 using ERPAccounting.Application.Validators;
-using ERPAccounting.Infrastructure.Data;
 using ERPAccounting.Infrastructure.Extensions;
-using ERPAccounting.Infrastructure.Repositories;
 using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,14 +18,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDocumentService, DocumentService>();
 builder.Services.AddScoped<IDocumentCostService, DocumentCostService>();
 builder.Services.AddScoped<IDocumentLineItemService, DocumentLineItemService>();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("ERPAccounting"));
-builder.Services.AddScoped<IDocumentLineItemRepository, DocumentLineItemRepository>();
-builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IValidator<CreateLineItemDto>, CreateLineItemValidator>();
 builder.Services.AddScoped<IValidator<PatchLineItemDto>, PatchLineItemValidator>();
-builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
